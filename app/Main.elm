b7a03e68953bd40729e6da6012e672adf2a17f93
Tikhon Belousko <dazmail01@gmail.com>
2017-01-22 23:37:03 +0300

    Add simple card markup.

--- app/Main.elm
+++ app/Main.elm
@@ -1,4 +1,47 @@
-import Html exposing (text)
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import Html.Attributes exposing (class)
+import List exposing (range, map)
 
 main =
-  text "Elm is talking to you from webpack 2.2!"
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+
+-- MODEL
+
+type alias Model = Int
+
+model : Model
+model =
+  0
+
+
+-- UPDATE
+
+type Msg = Increment | Decrement
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =  div []
+  (map (\i -> card) (range 1 16))
+
+card : Html Msg
+card = div [ class "card" ] [ text "🤣" ]
+
+
+  --div []
+  --  [ button [ onClick Decrement ] [ text "-" ]
+  --  , div [] [ text (toString model) ]
+  --  , button [ onClick Increment ] [ text "+" ]
+  --  , text "💪"
+  --  ]
