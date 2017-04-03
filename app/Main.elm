5f399adf9cff60e8259a426e235df005f9ef358b
Tikhon Belousko <dazmail01@gmail.com>
2017-04-03 13:33:53 +0300

    Restyle card back.

--- app/Main.elm
+++ app/Main.elm
@@ -142,7 +142,7 @@ card : Bool -> Card -> Html Msg
 card flipped c = div [ class (if flipped then "card--flipped" else "card"), onClick (FlipCard c) ]
   [ div [ class "card__content" ]
     [ div [ class "card__front"] [ text c.value ]
-    , div [ class "card__back"] [ text "👄" ]
+    , div [ class "card__back"] [ text "🎀" ]
     ]
   ]
 
